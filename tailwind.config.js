8b64547bfcb6b353f715996c778f1f7c2dea9aa5
fnhnafiz <nafsinsifan2k19@gmail.com>
2024-07-31 21:43:57 +0600

    update a section

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        dark3: "#777777",
+      },
+    },
+    plugins: [],
+  },
+};
